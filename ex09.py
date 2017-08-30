2d00119769828a7a47a752652a5336e4ed2fb3d3
James Tribiano <james.tribiano@gmail.com>
2017-08-30 15:11:26 -0400

    Added finished versions of ex09.py and ex10.py.

---
+++ ex09.py
@@ -0,0 +1,16 @@
+#Not finished yet.
+
+#example #09, coding the hard way - python.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMa\nJun\nJul\nAug"
+
+print "Here are the days:", days
+print "Here are the months:", months 
+
+print """
+There's something goig on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
